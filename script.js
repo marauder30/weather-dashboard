54432b294b1f30f3293c511404b04ad44bd49bff
Nick Koch <marauder30@gmail.com>
2020-01-02 18:57:35 -0700

    added uv index for home

--- script.js
+++ script.js
@@ -40,8 +40,12 @@ var date5 = $(`#date5`);
 var icon5 = $(`#icon5`);
 var temp5 = $(`#temp5`);
 var humid5= $(`#humid5`);
+var lat;
+var lon;
+var uvidx;
 
 
+init();
 
 
 function setCityName() {
@@ -50,9 +54,11 @@ function setCityName() {
 
 
 function init() {
+    // getLocation();
     setCityName();
     getWeather();
-    getLocation();
+    uvIndex();
+    
     
 }
 
@@ -69,52 +75,89 @@ function init() {
 function getWeather() {
     
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Denver&units=imperial&appid=b4e24afa7b1b97b59d4ac32e97c8b68d";
-
+    
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-
+        
         $(`p`).text("");
-        temp.text(`Current Temperature: ` + Math.floor(response.main.temp) + ` F`);
+        temp.text(`Current Temperature: ` + Math.floor(response.main.temp) + ` °F`);
         humid.text(`Humidity: ` + response.main.humidity);
         wind.text(`Wind Speed: ` + response.wind.speed);
     })
 }
 
-function getLocation () {
-   
-    var location = navigator.geolocation.getCurrentPosition(success, error);
-    console.log(location);
-    const status = $(`#status`);
-    const mapLink = $(`#map-link`);
-  
-    mapLink.href = '';
-    mapLink.textContent = '';
-  
-    function success(position) {
-      const latitude  = position.coords.latitude.toFixed(3);
-      const longitude = position.coords.longitude.toFixed(3);
-      console.log(latitude);
-      console.log(longitude);
-  
-      status.textContent = '';
-      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
-    }
-
-    function error() {
-        status.textContent = 'Unable to retrieve your location';
-      }
+// function getLocation () {
     
-      if (!navigator.geolocation) {
-        status.textContent = 'Geolocation is not supported by your browser';
-      } else {
-        status.textContent = 'Locating…';
-        navigator.geolocation.getCurrentPosition(success, error);
-      }
+//     const status = $(`#status`);
+//     const mapLink = $(`#map-link`);
+    
+//     mapLink.href = '';
+//     mapLink.textContent = '';
+    
+//     function success(position) {
+//         var location = navigator.geolocation.getCurrentPosition(success, error);
+//         var latitude  = position.coords.latitude.toFixed(2);
+//         var longitude = position.coords.longitude.toFixed(2);
+        
+//         status.textContent = '';
+//         mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+//         mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+        
+//         lat = latitude;
+//         lon = longitude;
+//         console.log(lat);
+//         console.log(lon);
+//         uvIndex();
+//     }
+    
+//     function error() {
+//         status.textContent = 'Unable to retrieve your location';
+//     }
+    
+//     if (!navigator.geolocation) {
+//         status.textContent = 'Geolocation is not supported by your browser';
+//     } else {
+//         status.textContent = 'Locating…';
+//         navigator.geolocation.getCurrentPosition(success, error);
+//     }
+    
+    
+    
+// }
 
+function uvIndex() {
+
+    var lat = "39.59";
+    var lon = "-104.99"
+
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=b4e24afa7b1b97b59d4ac32e97c8b68d";
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        
+        console.log(response.value);
+
+        uvidx = response.value;
+
+        uv.text(`UV Index: ` + uvidx);
+
+        console.log(uv);
+
+
+
+    })
 }
+
+
+
+
+
+
 // ajax callback function to grab 5 day forecast!
 
 
@@ -122,7 +165,6 @@ function getLocation () {
 
 // run functions 
 
-init();
 
 
 
